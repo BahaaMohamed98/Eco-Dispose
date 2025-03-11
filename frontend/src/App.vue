2395ff12429c4e3f9df0eea4164285bfa85a1efc
BahaaMohamed98 <emv23550@gmail.com>
2025-03-11 16:41:26 +0200

    Render footer conditionally

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,22 @@
 <script setup>
 import NavBar from "@/components/NavBar.vue";
 import Footer from "@/components/Footer.vue";
+import router from "@/router/routes.js";
+import { computed } from "vue";
+
+const footerExcludePaths = ["/login", "/register"];
+
+const renderFooter = computed(
+  () => !footerExcludePaths.includes(router.currentRoute.value.path),
+);
 </script>
 
 <template>
-    <NavBar></NavBar>
+  <NavBar></NavBar>
 
-    <router-view></router-view>
+  <router-view></router-view>
 
-    <Footer></Footer>
+  <Footer v-if="renderFooter"></Footer>
 </template>
 
 <style scoped></style>
