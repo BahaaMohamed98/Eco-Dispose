02755d11471b95eb665857fa63cb02b1469d4843
BahaaMohamed98 <emv23550@gmail.com>
2025-04-18 02:10:38 +0200

    feat(api): use CORS

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,6 +2,7 @@ import os
 
 from dotenv import load_dotenv
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 from .auth import login_manager
 from .config import UPLOAD_FOLDER
@@ -18,7 +19,14 @@ def create_app():
     app.config["MAX_CONTENT_LENGTH"] = 16 * 1000 * 1000
 
     app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
+    app.config['SESSION_COOKIE_SECURE'] = False
+    app.config['SESSION_COOKIE_HTTPONLY'] = True
     db.init_app(app)
+
+    CORS(app, origins=["http://localhost:3000"], supports_credentials=True)
 
     login_manager.init_app(app)
 
